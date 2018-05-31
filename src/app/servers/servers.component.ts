87832f0dcbbe0df13a52227147fc38e713e6c3e6
Uddip Mitra <uddipmitra@gmail.com>
2018-05-31 00:16:10 -0400

    Changed selector to attribute using square brackets

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,10 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
-  template: `
-  <app-server></app-server>
-  <app-server></app-server>`,
+  selector: '[app-servers]',
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
