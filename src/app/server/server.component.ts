999889fb15f15828996c919d97b68fc280c4808e
Uddip Mitra <uddipmitra@gmail.com>
2018-06-02 18:20:14 -0400

    Styling elements using ngClass to assign a style class conditionally

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,8 +1,13 @@
 import { Component } from "@angular/core";
 
 @Component({
-    selector: 'app-server',
-    templateUrl: './server.component.html'
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  styles: [`
+    .online {
+      color: white;
+    }
+  `]
 })
 export class ServerComponent {
     serverId: number = 10;
