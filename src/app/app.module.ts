c3108e7a26c79c04108029006fb2202410eaee78
Uddip Mitra <uddipmitra@gmail.com>
2018-06-02 19:35:19 -0400

    Added assignment 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import {FormsModule} from '@angular/forms';
+import {DispComponent} from './disp/disp.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    DispComponent
   ],
   imports: [
     BrowserModule,
