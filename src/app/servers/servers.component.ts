227b6122b7c42ec2e32462370aa35abfcc41d226
Uddip Mitra <uddipmitra@gmail.com>
2018-06-02 17:48:53 -0400

    ngIf and the else condition

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -9,7 +9,8 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
   serverName = '';
-  username = '';
+  serverCreated = false;
+
 
   constructor() {
     setTimeout(() => {
@@ -21,6 +22,7 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
+    this.serverCreated = true;
     this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
   }
 
