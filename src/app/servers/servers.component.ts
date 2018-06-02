3e968d4ccfb25ec46784850c77ab2fd201c8d93b
Uddip Mitra <uddipmitra@gmail.com>
2018-06-02 15:17:50 -0400

    Handling events

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -7,14 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = '';
 
-  constructor() { 
+  constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
     }, 2000);
   }
 
   ngOnInit() {
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created!';
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 
 }
